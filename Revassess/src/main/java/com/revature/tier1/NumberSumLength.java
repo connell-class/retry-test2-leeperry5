e556088f7aaaa99f8e0a8a185e7a5f6af95a2bbb
Perry Lee <perrylee_2004@yahoo.com>
2020-07-01 12:10:05 -0400

    Task 1

--- Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
+++ Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
@@ -3,6 +3,16 @@ package com.revature.tier1;
 public class NumberSumLength {
 
 	public static boolean checkNumberPowerLength(long num) {
-		return false;
+		// Here's hoping that this implementation fails for some (sufficiently large) inputs
+		double length = Math.floor(Math.log10((double)num)) + 1;
+		double sum = 0.0;
+		long tmp = num;
+		while (tmp != 0) {
+			double digit = (double)(tmp % 10);
+			sum += Math.pow(digit, length);
+			tmp /= 10;
+		}
+
+		return (long)sum == num;
 	}
 }
--- Revassess/src/main/java/com/revature/tier1/User.java
+++ Revassess/src/main/java/com/revature/tier1/User.java
@@ -1,4 +1,67 @@
 package com.revature.tier1;
 
 public class User {
+	// Let's try declaring these in a different order to make sure that the tests still work
+	private int id;
+	private String role = "";
+	private String lastName;
+	private String firstName;
+	private String username;
+	private String password;
+	
+	public User() {
+		id = 0;
+		firstName = "";
+		lastName = "";
+		username = "";
+		password = "";
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
